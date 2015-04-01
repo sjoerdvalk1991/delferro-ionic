2304e790c9e9a99e43c2f3b2b988f2ab31fc56d5
Sjoerd Valk <sjoerd@ivaldi.nl>
2015-04-01 16:26:00 +0200

    fix timeformat

--- myApp/www/js/states/results/overviewcontroller.js
+++ myApp/www/js/states/results/overviewcontroller.js
@@ -4,10 +4,11 @@ var overviewController = function($rootScope, $scope, $ionicPopup, params){
   var _this = this;
 
   this.catchWeek = function(params){
+  	console.log(params);
   	var results = JSON.parse(localStorage.getItem('dailyData'));
   	var i = 0;
     for (; i < results.length; i++) {
-      if(results[i].date == params){
+      if(results[i].date == params.date){
  					var equal = i;
       }
     }
@@ -16,6 +17,30 @@ var overviewController = function($rootScope, $scope, $ionicPopup, params){
     }else{
     	var weekData = results.splice(equal, 7); 
     }
+    j = 0;
+
+    dateArray = [];
+    for (; j < weekData.length; j++) {
+     	var date = weekData[j].date;
+     	dateFormat(date);
+    }
+    function dateFormat(date){
+    	var year = date.substr(6,4);
+  		var month = date.substr(3,2);
+  		var day = date.substr(0,2);
+  		dailyDate = {
+  			date: year+'-'+month+'-'+day
+  		}
+ 
+  		dateArray.push(dailyDate);
+  	}
+
+  	g = 0;	
+  	for (; g < weekData.length; g++) {
+     	weekData[g].date = dateArray[g].date;
+    }
+  	
+    console.log(weekData);
     _this.drawGraph(weekData);  	
   }
 
@@ -25,38 +50,37 @@ var overviewController = function($rootScope, $scope, $ionicPopup, params){
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-		var parseDate = d3.time.format("%d-%m-%Y").parse;
+		var parseDate = d3.time.format("%Y-%m-%d").parse;
 
 
 		var x = d3.time.scale()
-		    .range([0, width])
+	    .range([0, width])
 
 		var y = d3.scale.linear()
-		    .range([height, 0]);
+	    .range([height, 0]);
 
 		var xAxis = d3.svg.axis()
-		    .scale(x)
-		    .orient("bottom");
+	    .scale(x)
+	    .orient("bottom");
 
 		var yAxis = d3.svg.axis()
-		    .scale(y)
-		    .orient("left");
+	    .scale(y)
+	    .orient("left");
 
 		var line = d3.svg.line()
-		    .x(function(d) { return x(d.date); })
-		    .y(function(d) { return y(d.stutter); });
+	    .x(function(d) { return x(d.date); })
+	    .y(function(d) { return y(d.stutter); });
 
 		var svg = d3.select("body").append("svg")
-		    .attr("width", width + margin.left + margin.right)
-		    .attr("height", height + margin.top + margin.bottom)
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
 		  .append("g")
-		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 		  var data = arrData.map(function(d) {
-		      return {
-		         date: parseDate(d[0]),
-		         stutter: d[1]
-		      };
+	      return {
+	        date: parseDate(d[0]),
+	        stutter: d[1]
+	      };
 		      
 		  });
 
@@ -64,7 +88,7 @@ var overviewController = function($rootScope, $scope, $ionicPopup, params){
 
 
 	  x.domain(d3.extent(data, function(d) { return d.date; }));
-	  y.domain(d3.extent(data, function(d) { return d.close; }));
+	  y.domain(d3.extent(data, function(d) { return d.stutter; }));
 
 	  svg.append("g")
 	      .attr("class", "x axis")
@@ -87,7 +111,7 @@ var overviewController = function($rootScope, $scope, $ionicPopup, params){
 	      .attr("d", line);
 	};
 
-  this.catchWeek();
+  this.catchWeek(params);
 };
 
 overviewController.$inject = ['$rootScope', '$scope', '$ionicPopup', '$stateParams'];
